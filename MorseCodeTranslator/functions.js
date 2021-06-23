ff059d7cf20c697c6eec28afdf496dee63f03b71
Cara Jane Tran <carajanetran@Macbook-2.local>
2021-06-23 17:09:51 +1000

    modified code format

--- MorseCodeTranslator/functions.js
+++ MorseCodeTranslator/functions.js
@@ -1,75 +1,71 @@
 const forwardTranslation = {
-    a: ".-",
-    b: "-...",
-    c: "-.-.",
-    d: "-..",
-    e: ".",
-    f: "..-.",
-    g: "--.",
-    h: "....",
-    i: "..",
-    j: ".---",
-    k: "-.-",
-    l: ".-..",
-    m: "--",
-    n: "-.",
-    o: "---",
-    p: ".--.",
-    q: "--.-",
-    r: ".-.",
-    s: "...",
-    t: "-",
-    u: "..-",
-    v: "...-",
-    w: ".--",
-    x: "-..-",
-    y: "-.--",
-    z: "--..",
-    1: ".----",
-    2: "..---",
-    3: "...--",
-    4: "....-",
-    5: ".....",
-    6: "-....",
-    7: "--...",
-    8: "---..",
-    9: "----.",
-    0: "-----"
-}
+  a: ".-",
+  b: "-...",
+  c: "-.-.",
+  d: "-..",
+  e: ".",
+  f: "..-.",
+  g: "--.",
+  h: "....",
+  i: "..",
+  j: ".---",
+  k: "-.-",
+  l: ".-..",
+  m: "--",
+  n: "-.",
+  o: "---",
+  p: ".--.",
+  q: "--.-",
+  r: ".-.",
+  s: "...",
+  t: "-",
+  u: "..-",
+  v: "...-",
+  w: ".--",
+  x: "-..-",
+  y: "-.--",
+  z: "--..",
+  1: ".----",
+  2: "..---",
+  3: "...--",
+  4: "....-",
+  5: ".....",
+  6: "-....",
+  7: "--...",
+  8: "---..",
+  9: "----.",
+  0: "-----",
+};
 
-const reverseTranslation = Object.entries(forwardTranslation).reduce((acc, [key, value]) => {
+const reverseTranslation = Object.entries(forwardTranslation).reduce(
+  (acc, [key, value]) => {
     acc[value] = key;
     return acc;
-}, {});
-
-
+  },
+  {}
+);
 
 export const englishToMorse = (textInput) => {
-    const words = textInput.toLowerCase().split(""); //e.x ["w", "e", "l", "c", "o", "m", "e"]
-    const translatedArray = words.map((char) => {
-        if (char === " ") {
-            return "    ";
-        }
-        return forwardTranslation[char];
-    });
-    return translatedArray.join(" ");
-} 
+  const words = textInput.toLowerCase().split(""); //e.x ["w", "e", "l", "c", "o", "m", "e"]
+  const translatedArray = words.map((char) => {
+    if (char === " ") {
+      return "    ";
+    }
+    return forwardTranslation[char];
+  });
+  return translatedArray.join(" ");
+};
 // console.log(englishToMorse("hello world"));
 
-
-
-            
 export const morseToEnglish = (textInput) => {
-    const words = textInput.split("    "); //e.x ["- .... ." "-.-. .- -"]
-    const translatedArray = words.map((word) => {
-        const chars = word.split(" "); //e.x {["-" "..." "."] ["-.-." ".-" "-"]}
-        const translatedCharsArray = chars.map(char => {
-            return reverseTranslation[char];
-        });
-        return translatedCharsArray.join("");
+  const words = textInput.split("    "); //e.x ["- .... ." "-.-. .- -"]
+  const translatedArray = words.map((word) => {
+    const chars = word.split(" "); //e.x {["-" "..." "."] ["-.-." ".-" "-"]}
+    const translatedCharsArray = chars.map((char) => {
+      return reverseTranslation[char];
     });
-    return translatedArray.join(" ");
-} 
+    return translatedCharsArray.join("");
+  });
+  return translatedArray.join(" ");
+};
 // console.log(morseToEnglish("- .... .    -.-. .- -"));
-
-
